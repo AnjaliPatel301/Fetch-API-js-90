453f1b81a2a75ea3a2166b21a139f053561a2823
Anjali <sj5065070@gmail.com>
2025-06-03 21:05:11 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,48 @@
+const searchInput=document.querySelector('#search')
+
+async function fetchData() {
+    const record= await fetch('https://dummyjson.com/products');
+
+    const data=await record.json();
+    
+     console.log(data.products.map((value)=>{
+        return value.category;
+    })
+);
+    displayproduct(data.products)
+
+
+
+searchInput.addEventListener('input', ()=>{
+      const filterInput=data.products.filter((item)=>{
+
+      return item.category.
+      toLowerCase().
+      includes(searchInput.value.toLowerCase())
+      });
+
+      displayproduct(filterInput)
+})
+}
+
+function displayproduct(products){
+    let Html=""
+    products.forEach((item)=>{
+        
+         Html+=`<div class="card">
+         <img src=${item.images[0]} alt="">
+         <h3>${item.title}</h3>
+         <h4>${item.price}</h4>
+         <p>${item.description}</p>
+         </div>`
+
+    })
+
+    document.querySelector('.container-hero').innerHTML=Html;
+
+}
+
+
+fetchData()
+
 
